2a8bd9cbbd7044d65e7700bed993683bfe39299b
FranEspinel <fran.espinel@gmail.com>
2023-10-28 11:22:33 -0300

    Actualizacion tablas y creacion de procedimientos

--- Procedimientos.sql
+++ Procedimientos.sql
@@ -4,7 +4,7 @@ create procedure cargar_imperios(
 as begin
 insert into Imperios(nombre,TempPromedio) values (@Nombre, @TempPromedio)
 end;
-
+ 
 exec cargar_imperios 'Federacion unida de planetas','60'
 exec cargar_imperios 'Imperio estelar romulano','58'
 exec cargar_imperios 'Imperio Klingon','63'
