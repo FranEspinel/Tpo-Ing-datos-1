7c72eda3a5500d97091b030bee490bea42ab5fe3
FranEspinel <fran.espinel@gmail.com>
2023-10-28 20:00:13 -0300

    Cambios

--- Procedimientos.sql
+++ Procedimientos.sql
@@ -8,9 +8,9 @@ end;
 exec cargar_imperios 'Federacion unida de planetas','60'
 exec cargar_imperios 'Imperio estelar romulano','58'
 exec cargar_imperios 'Imperio Klingon','63'
-exec cargar_imperios 'Alianza ferengi','64'
-exec cargar_imperios 'Colectividad borg','55'
-exec cargar_imperios 'Union cardassiana','61'
+--exec cargar_imperios 'Alianza ferengi','64'
+--exec cargar_imperios 'Colectividad borg','55'
+--exec cargar_imperios 'Union cardassiana','61'
 ;
 ----------------------------------------------------------------------------------------
 create procedure cargar_flotas(
@@ -57,8 +57,42 @@ end;
 
 exec cargar_razas '','','','','',''
 ----------------------------------------------------------------------------------------
-create procedure cargar_capitanes()
+create procedure cargar_capitanes(
+@Nombre varchar(50),
+@id_imperio int,
+@id_nave int,
+@id_raza int,
+@planetanacimiento varchar(100),
+@inteligencia float,
+@coraje float
+)
+as begin
+insert into Capitanes(Nombre,ID_imperio,id_nave,id_raza,planetanacimiento,inteligencia,coraje)
+values (@nombre,@id_imperio,@id_nave, @id_raza, @planetanacimiento,@inteligencia,@coraje)
 
+end
+;
 
 ----------------------------------------------------------------------------------------
-create procedure cargar_naves()
+create procedure cargar_naves(
+@id_nave int,
+@Nombre varchar(50),
+@ID_flota int,
+@ID_imperio int,
+@ID_capitan int,
+@Vida float,
+@Maniobras varchar(100),
+@CapacidadDefensa float,
+@CapacidadAtque float,
+@EnergiaMax float ,
+@EnergiaAcumulada float, 
+@EnergiaManiobra float,
+@velmax float
+)
+as begin
+
+insert into naves(id_nave,nombre,id_flota,id_imperio,id_capitan,vida,maniobras,capacidaddefensa,capacidadataque,energiamax,energiaacumulada,energiamaniobra,velmax)
+values(@id_nave,@nombre,@id_flota,@id_imperio,@id_capitan,@vida,@maniobras,@capacidaddefensa,@capacidadataque,@energiamax,@energiaacumulada,@energiamaniobra,@velmax)
+
+end
+;
