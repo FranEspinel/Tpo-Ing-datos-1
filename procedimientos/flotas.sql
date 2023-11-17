9de7ff74e92b8825df2f123d14b25d7be9cea0af
FranEspinel <fran.espinel@gmail.com>
2023-11-17 20:32:58 -0300

    Se agregan datos a los exec

--- procedimientos/flotas.sql
+++ procedimientos/flotas.sql
@@ -8,17 +8,17 @@ as begin
     insert into Flotas(Nombre,ID_imperio,Ultima_accion, coordenadas) values (@Nombre,@id_imperio,@Ultima_accion,@coordenadas)
 end;
 
-exec cargar_flotas 'Flota FUP 1',1
-exec cargar_flotas 'Flota FUP 2',1
-exec cargar_flotas 'Flota FUP 3',1
+exec cargar_flotas 'Flota FUP 1',1,null,39
+exec cargar_flotas 'Flota FUP 2',1,null,39
+exec cargar_flotas 'Flota FUP 3',1,null,40
 
-exec cargar_flotas 'Flota Romulana 1',2
-exec cargar_flotas 'Flota Romulana 2',2
+exec cargar_flotas 'Flota Romulana 1',2,null,75
+exec cargar_flotas 'Flota Romulana 2',2,null,74
 
-exec cargar_flotas 'Flota Klingon 1',3
+exec cargar_flotas 'Flota Klingon 1',3,null,13
 
-exec cargar_flotas 'Flota Ferengi 1',4
+exec cargar_flotas 'Flota Ferengi 1',4,null,25
 
-exec cargar_flotas 'Flota Borg 1',5
+exec cargar_flotas 'Flota Borg 1',5,null,99
 
-exec cargar_flotas 'Flota Cardassiana 1',6
+exec cargar_flotas 'Flota Cardassiana 1',6,null,116
