d3036a07c2b0440b06094318c40bb2b3924ff2b8
Enzo Cazenave <encaze24@hotmail.com>
2023-11-22 13:47:07 -0300

    Cambios en razas_en_planetas

--- procedimientos/acciones_de_flotas.sql
+++ procedimientos/acciones_de_flotas.sql
@@ -5,22 +5,22 @@ create procedure cargar_acciones_de_flotas (
     INSERT INTO acciones_de_flotas(ID_accion, ID_flota) VALUES (@ID_accion, @ID_flota)
 END;
 
-exec cargar_acciones_de_flotas '1','1'
-exec cargar_acciones_de_flotas '1','2'
-exec cargar_acciones_de_flotas '1','3'
-exec cargar_acciones_de_flotas '1','4'
-exec cargar_acciones_de_flotas '1','5'
-exec cargar_acciones_de_flotas '1','6'
-exec cargar_acciones_de_flotas '1','7'
-exec cargar_acciones_de_flotas '1','8'
-exec cargar_acciones_de_flotas '1','9'
+exec cargar_acciones_de_flotas 1,1
+exec cargar_acciones_de_flotas 1,2
+exec cargar_acciones_de_flotas 1,3
+exec cargar_acciones_de_flotas 1,4
+exec cargar_acciones_de_flotas 1,5
+exec cargar_acciones_de_flotas 1,6
+exec cargar_acciones_de_flotas 1,7
+exec cargar_acciones_de_flotas 1,8
+exec cargar_acciones_de_flotas 1,9
 
-exec cargar_acciones_de_flotas '2','1'
-exec cargar_acciones_de_flotas '2','2'
-exec cargar_acciones_de_flotas '2','3'
-exec cargar_acciones_de_flotas '2','4'
-exec cargar_acciones_de_flotas '2','5'
-exec cargar_acciones_de_flotas '2','6'
-exec cargar_acciones_de_flotas '2','7'
-exec cargar_acciones_de_flotas '2','8'
-exec cargar_acciones_de_flotas '2','9'
+exec cargar_acciones_de_flotas 2,1
+exec cargar_acciones_de_flotas 2,2
+exec cargar_acciones_de_flotas 2,3
+exec cargar_acciones_de_flotas 2,4
+exec cargar_acciones_de_flotas 2,5
+exec cargar_acciones_de_flotas 2,6
+exec cargar_acciones_de_flotas 2,7
+exec cargar_acciones_de_flotas 2,8
+exec cargar_acciones_de_flotas 2,9
--- procedimientos/razas_en_planetas.sql
+++ procedimientos/razas_en_planetas.sql
@@ -1,46 +1,46 @@
 create procedure cargar_razas_en_planetas (
     @ID_raza int,
-    @ID_planeta int,
+    @ID_planeta varchar(100),
     @Porcentaje_planeta float
 ) AS BEGIN
     INSERT INTO razas_en_planetas(ID_raza, ID_planeta, Porcentaje_planeta) VALUES (@ID_raza, @ID_planeta, @Porcentaje_planeta)
 END;
 
-exec cargar_razas_en_planetas '1','1',80.0
-exec cargar_razas_en_planetas '3','1',5.0
-exec cargar_razas_en_planetas '4','1',5.0
-exec cargar_razas_en_planetas '6','1',5.0
-exec cargar_razas_en_planetas '7','1',5.0
+exec cargar_razas_en_planetas 1,'FUP001',80.0
+exec cargar_razas_en_planetas 3,'FUP001',5.0
+exec cargar_razas_en_planetas 4,'FUP001',5.0
+exec cargar_razas_en_planetas 6,'FUP001',5.0
+exec cargar_razas_en_planetas 7,'FUP001',5.0
 
-exec cargar_razas_en_planetas '1','2',15.0
-exec cargar_razas_en_planetas '6','2',2.5
-exec cargar_razas_en_planetas '7','2',82.5
+exec cargar_razas_en_planetas 1,'FUP002',15.0
+exec cargar_razas_en_planetas 6,'FUP002',2.5
+exec cargar_razas_en_planetas 7,'FUP002',82.5
 
-exec cargar_razas_en_planetas '1','3',25.0
-exec cargar_razas_en_planetas '2','3',25.0
-exec cargar_razas_en_planetas '3','3',25.0
-exec cargar_razas_en_planetas '4','3',25.0
+exec cargar_razas_en_planetas 1,'FUP003',25.0
+exec cargar_razas_en_planetas 2,'FUP003',25.0
+exec cargar_razas_en_planetas 3,'FUP003',25.0
+exec cargar_razas_en_planetas 4,'FUP003',25.0
 
-exec cargar_razas_en_planetas '1','4',25.0
-exec cargar_razas_en_planetas '2','4',25.0
-exec cargar_razas_en_planetas '3','4',25.0
-exec cargar_razas_en_planetas '4','4',25.0
+exec cargar_razas_en_planetas 1,'FUP004',25.0
+exec cargar_razas_en_planetas 2,'FUP004',25.0
+exec cargar_razas_en_planetas 3,'FUP004',25.0
+exec cargar_razas_en_planetas 4,'FUP004',25.0
 
-exec cargar_razas_en_planetas '1','5',1.0
-exec cargar_razas_en_planetas '2','5',98.0
-exec cargar_razas_en_planetas '3','5',1.0
+exec cargar_razas_en_planetas 1,'IR001',1.0
+exec cargar_razas_en_planetas 2,'IR001',98.0
+exec cargar_razas_en_planetas 3,'IR001',1.0
 
-exec cargar_razas_en_planetas '1','6',10.0
-exec cargar_razas_en_planetas '3','6',85.0
-exec cargar_razas_en_planetas '4','6',5.0
+exec cargar_razas_en_planetas 1,'IK001',10.0
+exec cargar_razas_en_planetas 3,'IK001',85.0
+exec cargar_razas_en_planetas 4,'IK001',5.0
 
-exec cargar_razas_en_planetas '1','7',7.5
-exec cargar_razas_en_planetas '3','7',90.0
-exec cargar_razas_en_planetas '4','7',2.5
+exec cargar_razas_en_planetas 1,'IK002',7.5
+exec cargar_razas_en_planetas 3,'IK002',90.0
+exec cargar_razas_en_planetas 4,'IK002',2.5
 
-exec cargar_razas_en_planetas '1','8',7.5
-exec cargar_razas_en_planetas '2','8',1.0
-exec cargar_razas_en_planetas '3','8',1.5
-exec cargar_razas_en_planetas '4','8',80
-exec cargar_razas_en_planetas '6','8',8.0
-exec cargar_razas_en_planetas '7','8',2.0
+exec cargar_razas_en_planetas 1,'AF001',7.5
+exec cargar_razas_en_planetas 2,'AF001',1.0
+exec cargar_razas_en_planetas 3,'AF001',1.5
+exec cargar_razas_en_planetas 4,'AF001',80
+exec cargar_razas_en_planetas 6,'AF001',8.0
+exec cargar_razas_en_planetas 7,'AF001',2.0
