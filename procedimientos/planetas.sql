16157c0a684381979b3b5a1023f1da5c67bbdf03
FranEspinel <fran.espinel@gmail.com>
2023-11-22 11:23:53 -0300

    Cambios de montañas

--- procedimientos/planetas.sql
+++ procedimientos/planetas.sql
@@ -11,17 +11,17 @@ as begin
     values(@Nombre,@nombrevulgar,@id_imperio,@coordenadas,@nombremontania,@alturamontania)
 end;
 
-exec cargar_planetas 'FUP001','Tierra',1,'','Aconcagua',6961.0
-exec cargar_planetas 'FUP002','Betazed',1,'','Aconcagua',6961.0
-exec cargar_planetas 'FUP003','Trill',1,'','Aconcagua',6961.0
-exec cargar_planetas 'FUP004','Janus IV',1,'','Aconcagua',6961.0
+exec cargar_planetas 'FUP001','Tierra',1,'','Aconcagua',6961.3
+exec cargar_planetas 'FUP002','Betazed',1,'','Olimpo',10352.8
+exec cargar_planetas 'FUP003','Trill',1,'','Hades',4609.4
+exec cargar_planetas 'FUP004','Janus IV',1,'','K2',8611.1
 
 
 exec cargar_planetas 'IR001','Romulo',2,'','Everest',8849.0
 
 
-exec cargar_planetas 'IK001','Qo nos',3,'','Makalu',8481.0
-exec cargar_planetas 'IK002','Khitomer',3,'','Makalu',8481.0
+exec cargar_planetas 'IK001','Qo nos',3,'','Makalu',8481.5
+exec cargar_planetas 'IK002','Khitomer',3,'','Elgon',8081.2
 
 
-exec cargar_planetas 'AF001','Ferenginar',4,'','Aconcagua',6961.0
+exec cargar_planetas 'AF001','Ferenginar',4,'','Kanchenjunga',8586.9
